44f22560e3ded5dbfbdbe11205d7d59ad1f202af
Liam <liam.aharon@hotmail.com>
2017-04-27 21:29:30 +1000

    Clicking on sidebar link snaps to id in content

--- contentscript.js
+++ contentscript.js
@@ -34,9 +34,32 @@ if (applySinglePage) {
     // replace course with single page course
     $('.course').empty();
     $('.course').append(singlePageCourse);
+    // remove single page link
+    $('.course__sidebar-full-details').remove();
     // remove stupid sidebar
     // $('.layout-sidebar__side__inner').remove();
-  })
+
+    $('.layout-sidebar__side__inner a').each(function(){
+      text = this.innerHTML;
+      if (text.includes('Timetable')) {
+        $(this).text('Timetable (open in new tab)');
+      }
+      // special case as dates in content is spelt with '&' instead of 'and'
+      else if (text.includes('Dates')) {
+        foundin = $(`h2:contains("Dates & times")`);
+        $(foundin[0]).attr('id', text);
+        console.log(foundin[0]);
+        $(this).attr('href', `#${text}`);
+      }
+      // don't pickup on timetable
+      else {
+        foundin = $(`h2:contains("${text}")`);
+        $(foundin[0]).attr('id', text);
+        console.log(foundin[0]);
+        $(this).attr('href', `#${text}`);
+      }
+    })
+  });
 }
 
 // make sidebar scroll with user
@@ -44,14 +67,38 @@ window.addEventListener('scroll', function (evt) {
   // This value is your scroll distance from the top
   var distance_from_top = document.body.scrollTop;
   // stop scrolling, reached upper bound
-  if (distance_from_top < 260) {
+  if (distance_from_top < 223) {
     $('.layout-sidebar__side__inner').css("position", "absolute");
     $('.layout-sidebar__side__inner').css("top", "");
   }
 
   // make div follow user
-  if(distance_from_top >= 260) {
+  if(distance_from_top >= 223) {
     $('.layout-sidebar__side__inner').css("position", "fixed");
     $('.layout-sidebar__side__inner').css("top", "100px");
   }
 });
+
+
+
+
+
+// make snapping to anchor snap a little above it
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 40);
+  }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
