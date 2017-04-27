84974b19db9075e8e8cc41ff10c296de06b10b9d
Liam <liam.aharon@hotmail.com>
2017-04-27 15:19:37 +1000

    Sidebar info to follow user

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,4 @@
+
 const curUrl = window.location.href;
 const applySinglePage =
       // checks if page can be viewed in single page
@@ -34,6 +35,23 @@ if (applySinglePage) {
     $('.course').empty();
     $('.course').append(singlePageCourse);
     // remove stupid sidebar
-    $('.layout-sidebar__side').remove();
+    // $('.layout-sidebar__side__inner').remove();
   })
 }
+
+// make sidebar scroll with user
+window.addEventListener('scroll', function (evt) {
+  // This value is your scroll distance from the top
+  var distance_from_top = document.body.scrollTop;
+  // stop scrolling, reached upper bound
+  if (distance_from_top < 260) {
+    $('.layout-sidebar__side__inner').css("position", "absolute");
+    $('.layout-sidebar__side__inner').css("top", "");
+  }
+
+  // make div follow user
+  if(distance_from_top >= 260) {
+    $('.layout-sidebar__side__inner').css("position", "fixed");
+    $('.layout-sidebar__side__inner').css("top", "100px");
+  }
+});
