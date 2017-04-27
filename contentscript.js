e31ad1f04e07bfc4e0c357bfecb00925b024a694
Liam <liam.aharon@hotmail.com>
2017-04-27 10:11:30 +1000

    Slightly improved performance

--- contentscript.js
+++ contentscript.js
@@ -1,27 +1,28 @@
-shouldChangeUrl =
+const curUrl = window.location.href;
+const shouldChangeUrl =
       // checks if page can be viewed in single page
       document.getElementsByClassName("course__sidebar-full-details").length > 0 &&
       // check if we're already on single page view
-      !window.location.href.includes('/print') &&
+      !curUrl.includes('/print') &&
       // check if user has clicked subject specifics
-      !window.location.href.includes('/eligibility-and-requirements') &&
-      !window.location.href.includes('/assessment') &&
-      !window.location.href.includes('/dates-times') &&
-      !window.location.href.includes('/further-information') &&
+      !curUrl.includes('/eligibility-and-requirements') &&
+      !curUrl.includes('/assessment') &&
+      !curUrl.includes('/dates-times') &&
+      !curUrl.includes('/further-information') &&
       // check if user has clicked major specifics
-      !window.location.href.includes('/components') &&
-      !window.location.href.includes('/course-structure') &&
-      !window.location.href.includes('/subject-options') &&
+      !curUrl.includes('/components') &&
+      !curUrl.includes('/course-structure') &&
+      !curUrl.includes('/subject-options') &&
       // check if user has clicked course specifics (note there is some crossover with major)
-      !window.location.href.includes('/entry-participation-requirements') &&
-      !window.location.href.includes('/attributes-outcomes-skills') &&
-      !window.location.href.includes('/breadth-requirements') &&
-      !window.location.href.includes('/majors-minors-specialisations') &&
-      !window.location.href.includes('/further-study') &&
-      !window.location.href.includes('/notes');
+      !curUrl.includes('/entry-participation-requirements') &&
+      !curUrl.includes('/attributes-outcomes-skills') &&
+      !curUrl.includes('/breadth-requirements') &&
+      !curUrl.includes('/majors-minors-specialisations') &&
+      !curUrl.includes('/further-study') &&
+      !curUrl.includes('/notes');
 
 // if we're not already on single page view and can be in it transition, change url
 if (shouldChangeUrl) {
-  newUrl = window.location.href + "/print";
+  const newUrl = curUrl + "/print";
   window.location.replace(newUrl);
 }
