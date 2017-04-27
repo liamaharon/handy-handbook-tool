f7b45f0719c2be7240ff22f3b1ab910e30c5ae77
Liam <liam.aharon@hotmail.com>
2017-04-27 22:25:07 +1000

    Abstracted and cleaned up

--- contentscript.js
+++ contentscript.js
@@ -1,30 +1,8 @@
 // remove # anchor from url we're looking at so we get the single page content
 // from right place
 const curUrl = window.location.href.split('#')[0];
-const applySinglePage =
-      // checks if page can be viewed in single page
-      document.getElementsByClassName("course__sidebar-full-details").length > 0 &&
-      // check if we're already on single page view
-      !curUrl.includes('/print') &&
-      // check if user has clicked subject specifics
-      !curUrl.includes('/eligibility-and-requirements') &&
-      !curUrl.includes('/assessment') &&
-      !curUrl.includes('/dates-times') &&
-      !curUrl.includes('/further-information') &&
-      // check if user has clicked major specifics
-      !curUrl.includes('/components') &&
-      !curUrl.includes('/course-structure') &&
-      !curUrl.includes('/subject-options') &&
-      // check if user has clicked course specifics (note there is some crossover with major)
-      !curUrl.includes('/entry-participation-requirements') &&
-      !curUrl.includes('/attributes-outcomes-skills') &&
-      !curUrl.includes('/breadth-requirements') &&
-      !curUrl.includes('/majors-minors-specialisations') &&
-      !curUrl.includes('/further-study') &&
-      !curUrl.includes('/notes');
-
 // apply single page view
-if (applySinglePage) {
+if (doChanges(curUrl)) {
   const newUrl = curUrl + "/print";
   // get html from single page view page
   $.get(newUrl, function(singlePageHtml, status){
@@ -64,43 +42,34 @@ if (applySinglePage) {
   });
 }
 
-// make sidebar scroll with user
-window.addEventListener('scroll', function (evt) {
-  // This value is your scroll distance from the top
-  var distance_from_top = document.body.scrollTop;
-  // stop scrolling, reached upper bound
-  if (distance_from_top < 223) {
-    $('.layout-sidebar__side__inner').css("position", "absolute");
-    $('.layout-sidebar__side__inner').css("top", "");
+/***********************************************/
+// checks if we're on page that we can improve
+function doChanges(curUrl) {
+
+  // checks if page can be viewed in single page
+  if (
+  document.getElementsByClassName("course__sidebar-full-details").length > 0 &&
+  // check if we're already on single page view
+  !curUrl.includes('/print') &&
+  // check if user has clicked subject specifics
+  !curUrl.includes('/eligibility-and-requirements') &&
+  !curUrl.includes('/assessment') &&
+  !curUrl.includes('/dates-times') &&
+  !curUrl.includes('/further-information') &&
+  // check if user has clicked major specifics
+  !curUrl.includes('/components') &&
+  !curUrl.includes('/course-structure') &&
+  !curUrl.includes('/subject-options') &&
+  // check if user has clicked course specifics (note there is some crossover with major)
+  !curUrl.includes('/entry-participation-requirements') &&
+  !curUrl.includes('/attributes-outcomes-skills') &&
+  !curUrl.includes('/breadth-requirements') &&
+  !curUrl.includes('/majors-minors-specialisations') &&
+  !curUrl.includes('/further-study') &&
+  !curUrl.includes('/notes')
+  ) {
+      return true
   }
-
-  // make div follow user
-  if(distance_from_top >= 223) {
-    $('.layout-sidebar__side__inner').css("position", "fixed");
-    $('.layout-sidebar__side__inner').css("top", "100px");
-  }
-});
-
-
-
-
-
-// make snapping to anchor snap a little above it
-// The function actually applying the offset
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY - 40);
-  }
+  return false
 }
-
-// Captures click events of all <a> elements with href starting with #
-$(document).on('click', 'a[href^="#"]', function(event) {
-  // Click events are captured before hashchanges. Timeout
-  // causes offsetAnchor to be called after the page jump.
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
-});
-
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
+/***********************************************/
