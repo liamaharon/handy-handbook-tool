aeb184ff3471b1e159e3326bdc9029de4f2d7ef5
Liam <liam.aharon@hotmail.com>
2017-04-29 23:05:20 +1000

    Make point for sidebar to start scrolling more accurate

--- window.js
+++ window.js
@@ -1,18 +1,23 @@
 // make sidebar scroll with user
-$('.header--course-and-subject').append('<div id="scrollPoint"></div>');
-scrollHeight = $('#scrollPoint').offset().top
+// specify point to start scrolling
+$('.header--course-and-subject').append('<div id="startScroll"></div>');
+startScroll = $('#startScroll').offset().top
 window.addEventListener('scroll', function (evt) {
   // This value is your scroll distance from the top
   var distance_from_top = document.body.scrollTop;
   // stop scrolling, reached upper bound
-  if (distance_from_top < scrollHeight) {
+  if (distance_from_top < startScroll) {
     $('.layout-sidebar__side__inner').css("position", "absolute");
     $('.layout-sidebar__side__inner').css("top", "");
   }
 
   // make div follow user
-  if(distance_from_top >= scrollHeight) {
+  else if(distance_from_top >= startScroll) {
     $('.layout-sidebar__side__inner').css("position", "fixed");
     $('.layout-sidebar__side__inner').css("top", "100px");
   }
 });
+// todo: make sidebar stop scrolling when reached bottom of page
+// specify point to end scrolling
+// $('footer').prepend('<div id="endScroll"></div>');
+// endScroll = $('#endScroll').offset().top
